6de0c2de9573ccd3a09d65670c43aba538fc1e8c
Denis Alimov <de.alimov@mail.ru>
2022-02-26 10:50:56 +0300

    Tree set заменил на ArrayList, теперь с дополнительным значением работает за счет сортировки. PS: в задании не было запрета на использование TreeSet, и с исходным набором данных мой метод работал корректно, тк в нем не было повторений четных положительных значений:)

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Set<Integer> result = new TreeSet<>();
+        List<Integer> result = new ArrayList<>();
         for (int x : intList) {
             if (x <= 0 || x % 2 != 0) continue;
             result.add(x);
         }
+        Collections.sort(result);
         for (int x : result) {
             System.out.println(x);
         }
